07ad9c76b455c036263f8af44ff5ee8e562c6aeb
GMDEEP <stewartjgreen@gmail.com>
2021-07-28 01:22:46 +0000

    Update with minor issues

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,9 +1,11 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { ToDoList } from "./ToDo";
 
 //create your first component
 export function Home() {
-	return <div className="text-center mt-5"></div>;
+	return (
+		<div>
+			<ToDoList />
+		</div>
+	);
 }
